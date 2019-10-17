6a401bd6121a35ac454556332f468951c455464f
david115896 <dtaccori@gmail.com>
2019-10-17 23:10:21 +0200

    final

--- lib/views/boardcase.rb
+++ lib/views/boardcase.rb
@@ -6,17 +6,9 @@ $:.unshift File.expand_path('./../lib', __FILE__)
 
 class BoardCase
 	attr_accessor :box_name, :box_value	#2 varriables par boardcase, son nom et sa valeur (vide , x ou o)
-	#@@all_box_case=Array.new
 	def initialize(name)
 		@box_name = name	#le nom de la case est definit dans la class Board
 		@box_value = ""	
-	#	@@all_box_case << self
 	end	
-
-
-#	def self.all_boardcase_instances
-#		return  @@all_box_case
-#	end
-	
 
 end
